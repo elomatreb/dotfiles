8792d53025c915eae9d8b78df98e84d0c552ff34
Ole Bertram <git@bertr.am>
2018-05-16 14:42:35 +0200

    User-install npm globals

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,4 @@ set CHRUBY_ROOT /usr
 source /usr/local/share/chruby/chruby.fish
 chruby 2.5.1
 
-set PATH $HOME/.bin $HOME/.config/composer/vendor/bin $PATH
+set PATH $HOME/.bin $HOME/.config/composer/vendor/bin $HOME/.npm/bin $PATH
